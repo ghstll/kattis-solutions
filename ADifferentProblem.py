444df47ba920f6b7d38fee307545cc91d72d6d17
ghstll <129940439+ghstll@users.noreply.github.com>
2024-10-05 05:06:05 -0600

    Revert "Solved"
    This reverts commit 5be900ce580e65bd6bd09e8a48bd909c7a8b8858.

--- ADifferentProblem.py
+++
@@ -1,5 +0,0 @@
-#https://open.kattis.com/problems/different
-import sys
-for line in sys.stdin:
-    x,y = map(int,line.split())
-    print(abs(x-y))
--- ARealChallenge.py
+++
@@ -1,5 +0,0 @@
-#https://open.kattis.com/problems/areal
-import math 
-
-a = int(input())
-print(4*math.sqrt(a))
